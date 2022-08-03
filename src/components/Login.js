5cdccd8a4eba3cd10a274fdc98eac35b5bb4c2ca
NicksyRop <nicksonkipkorir25@gmail.com>
2022-08-03 10:38:38 +0300

    added test for onChange listeners

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 
 function Login() {
   const [error, setErr] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="username" value="" />
-        <input type="password" placeholder="password" value="" />
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button disabled="true">Login</button>
       </form>
       <span
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
 import Login from "./Login";
 
 test("username input is rendered", () => {
@@ -55,4 +56,27 @@ test("Error is not visible", () => {
   const errorMessage = screen.getByTestId("error");
 
   expect(errorMessage).not.toBeVisible();
+});
+
+test("username input should change", () => {
+  render(<Login />);
+
+  const userInput = screen.getByPlaceholderText(/username/i);
+
+  const testVal = "test";
+  fireEvent.change(userInput, { target: { value: testVal } });
+
+  expect(userInput.value).toBe(testVal);
+});
+
+test("password input should change", () => {
+  render(<Login />);
+
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  const testVal = "password";
+
+  fireEvent.change(passwordInput, { target: { value: testVal } });
+  expect(passwordInput.value).toBe(testVal);
+
+  expect(passwordInput).toBeInTheDocument();
 });
